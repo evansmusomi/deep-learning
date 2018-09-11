f8ad0de6ecbf2620197adb8cb6f3288b726c41ff
Evans Musomi <evans.musomi@gmail.com>
2018-09-11 09:24:58 +0300

    feat: finishes data preprocessing template

--- ml-basics/data-processing-template/data_preprocessing_template.py
+++ ml-basics/data-processing-template/data_preprocessing_template.py
@@ -10,21 +10,6 @@ dataset = pandas.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-# handle missing data
-from sklearn.preprocessing import Imputer
-imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
-imputer = imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
-
-# encode categorical data
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelencoder_X = LabelEncoder()
-X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
-onehotencoder = OneHotEncoder(categorical_features=[0])
-X = onehotencoder.fit_transform(X).toarray()
-labelencoder_y = LabelEncoder()
-y = labelencoder_y.fit_transform(y)
-
 # splitting the dataset into training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(
