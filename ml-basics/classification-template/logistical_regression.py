e8e060dc3b027ebf48a8eac5685ad71b9bf7b762
Evans Musomi <evans.musomi@gmail.com>
2018-09-11 13:43:37 +0300

    feat: visualizes training and test set logistical regression results

--- ml-basics/classification-template/logistical_regression.py
+++ ml-basics/classification-template/logistical_regression.py
@@ -20,3 +20,51 @@ from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)
+
+# fit logistic regression to the training set
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression(random_state=0)
+classifier.fit(X_train, y_train)
+
+# predict the test set results
+y_pred = classifier.predict(X_test)
+
+# make the confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# visualize the training set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = numpy.meshgrid(numpy.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                        numpy.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+pyplot.contourf(X1, X2, classifier.predict(numpy.array([X1.ravel(), X2.ravel()]).T).reshape(
+    X1.shape), alpha=0.75, cmap=ListedColormap(("red", "green")))
+pyplot.xlim(X1.min(), X1.max())
+pyplot.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    pyplot.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                   c=ListedColormap(("red", "green"))(i), label=j)
+pyplot.title("Logistic regression (training set)")
+pyplot.xlabel("Age")
+pyplot.ylabel("Estimated Salary")
+pyplot.legend()
+pyplot.show()
+
+# visualize the test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = numpy.meshgrid(numpy.arange(start=X_set[:, 0].min() - 1, stop=X_set[:, 0].max() + 1, step=0.01),
+                        numpy.arange(start=X_set[:, 1].min() - 1, stop=X_set[:, 1].max() + 1, step=0.01))
+pyplot.contourf(X1, X2, classifier.predict(numpy.array([X1.ravel(), X2.ravel()]).T).reshape(
+    X1.shape), alpha=0.75, cmap=ListedColormap(("red", "green")))
+pyplot.xlim(X1.min(), X1.max())
+pyplot.ylim(X2.min(), X2.max())
+for i, j in enumerate(numpy.unique(y_set)):
+    pyplot.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                   c=ListedColormap(("red", "green"))(i), label=j)
+pyplot.title("Logistic regression (test set)")
+pyplot.xlabel("Age")
+pyplot.ylabel("Estimated Salary")
+pyplot.legend()
+pyplot.show()
